a319af0a66fc928b9dac23ecd56fb81d4ae1e92c
Dan Boehmke <Daniel@webappclouds.com>
2019-12-05 11:24:26 -0500

    Initial Commit

---
+++ src/lib/data.js
@@ -0,0 +1,26 @@
+export const articles = [
+  {
+    title: 'New Tesla Truck',
+    imageUrl: 'https://cnet4.cbsistatic.com/img/p80i5gtOMko10w2pl0XLlwaRED4=/1092x0/2019/11/22/62d4e583-2039-4187-908b-8a7272175936/cybertruck-7.jpg',
+    description: 'This new truck is cyberific.',
+    date: new Date(),
+    author: 'Bill Smith',
+    url: 'https://www.cnet.com/roadshow/news/tesla-cybertruck-order-reservation-deposit/'
+  },
+  {
+    title: 'New Tesla Truck',
+    imageUrl: 'https://cnet4.cbsistatic.com/img/p80i5gtOMko10w2pl0XLlwaRED4=/1092x0/2019/11/22/62d4e583-2039-4187-908b-8a7272175936/cybertruck-7.jpg',
+    description: 'This new truck is cyberific.',
+    date: new Date(),
+    author: 'Bill Smith',
+    url: 'https://www.cnet.com/roadshow/news/tesla-cybertruck-order-reservation-deposit/'
+  },
+  {
+    title: 'New Tesla Truck',
+    imageUrl: 'https://cnet4.cbsistatic.com/img/p80i5gtOMko10w2pl0XLlwaRED4=/1092x0/2019/11/22/62d4e583-2039-4187-908b-8a7272175936/cybertruck-7.jpg',
+    description: 'This new truck is cyberific.',
+    date: new Date(),
+    author: 'Bill Smith',
+    url: 'https://www.cnet.com/roadshow/news/tesla-cybertruck-order-reservation-deposit/'
+  },
+];
