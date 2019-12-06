172b18bd4c5e172a134fe71144d7c7e5b1e50409
Dan Boehmke <Daniel@webappclouds.com>
2019-12-06 15:53:49 -0500

    Adding Search Text

--- src/containers/HomeContainer/HomeView/index.js
+++ src/containers/HomeContainer/HomeView/index.js
@@ -3,6 +3,7 @@ import { View, FlatList } from 'react-native';
 import PropTypes from 'prop-types';
 import Article from '../../../components/Article';
 import NewsModal from '../../../components/NewsModal';
+import Search from '../../../components/Search';
 import { styles } from './styles';
 import * as globals from '../../../lib/globals';
 
@@ -12,36 +13,68 @@ const HomeView = (props) => {
     isNewsModalVisible,
     isModalUrl,
     onModalClose,
-    news
+    news,
+    searchText,
+    setSearchText
   } = props;
+
+  let newsData = news;
+
+  if (searchText && searchText.trim() !== "") {
+    let dataAfterSearch = [];
+    for (let i = 0; i < newsData.length; i++) {
+      let author = newsData[i].author != null ? newsData[i].author.toLowerCase() : "";
+      let description = newsData[i].description != null ? newsData[i].description.toLowerCase() : "";
+      let title = newsData[i].title != null ? newsData[i].title.toLowerCase() : "";
+
+      let matchAuthor = author.indexOf(searchText.trim().toLowerCase());
+      let matchDescription = description.indexOf(searchText.trim().toLowerCase());
+      let matchTitle = title.indexOf(searchText.trim().toLowerCase());
+
+      if (matchAuthor !== -1 || matchDescription !== -1 || matchTitle !== -1) {
+        dataAfterSearch.push(newsData[i])
+      }
+    }
+    newsData = dataAfterSearch;
+  }
+
   return (
     <View style={globals.COMMON_STYLES.pageContainer}>
-      <FlatList
-        data={news}
-        showsVerticalScrollIndicator={false}
-        keyExtractor={item => item.url}
-        renderItem={({ item, ...rest }) => {
-          const index = parseInt(rest[1], 10)
-          return (
-            <Article
-              key={index}
-              date={item.date}
-              imageUrl={item.imageUrl}
-              title={item.title}
-              description={item.description}
-              author={item.author}
-              index={index}
-              onPress={() => onModalOpen(item.url)}
-              style={styles.newItem}
+      <Search searchText={searchText} setSearchText={setSearchText} />
+      {
+        newsData && newsData.length > 0 ? (
+          <View>
+            <FlatList
+              data={newsData}
+              showsVerticalScrollIndicator={false}
+              keyExtractor={item => item.url}
+              renderItem={({ item, ...rest }) => {
+                const index = parseInt(rest[1], 10)
+                return (
+                  <Article
+                    key={index}
+                    date={item.date}
+                    imageUrl={item.imageUrl}
+                    title={item.title}
+                    description={item.description}
+                    author={item.author}
+                    index={index}
+                    onPress={() => onModalOpen(item.url)}
+                    style={styles.newItem}
+                  />
+                )
+              }}
             />
+            <NewsModal
+              isNewsModalVisible={isNewsModalVisible}
+              onModalClose={onModalClose}
+              isModalUrl={isModalUrl}
+            />
+          </View>
+        ) : (
+            null
           )
-        }}
-      />
-      <NewsModal
-        isNewsModalVisible={isNewsModalVisible}
-        onModalClose={onModalClose}
-        isModalUrl={isModalUrl}
-      />
+      }
     </View>
   )
 }
@@ -52,7 +85,8 @@ HomeView.propTypes = {
   isNewsModalVisible: PropTypes.bool,
   isModalUrl: PropTypes.string,
   onModalClose: PropTypes.func,
-  
+  searchText: PropTypes.string.isRequired,
+  setSearchText: PropTypes.func.isRequired
 }
 
 export default HomeView;
